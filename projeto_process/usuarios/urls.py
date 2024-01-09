a0aff7610ba7123998fe9cc7c0c4c4938c7c37e6
Caique108 <caiquecouto108@gmail.com>
2024-01-09 11:46:22 -0300

    Autenticando botão de usuario

--- projeto_process/usuarios/urls.py
+++ projeto_process/usuarios/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
 
 
 urlpatterns = [
@@ -8,6 +9,8 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(
         template_name = 'usuarios/form.html'
         ), name="login"),
-    path('logout/',auth_views.LogoutView.as_view(), name="logout")
+
+    path('logout/',auth_views.LogoutView.as_view()
+    , name="logout")
 
 ]
