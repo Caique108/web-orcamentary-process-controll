7065b2b8acde81dae737e3e5f55dad98ee8db06c
Caique108 <caiquecouto108@gmail.com>
2024-01-16 16:01:43 -0300

    Corrigindo erro de Logout

--- projeto_process/usuarios/urls.py
+++ projeto_process/usuarios/urls.py
@@ -6,10 +6,10 @@ urlpatterns = [
 
     # path('', , name=''),
     path('login/', auth_views.LoginView.as_view(
-        template_name = 'usuarios/form.html'
-        ), name="login"),
+        template_name = 'usuarios/login.html'
+    ), name="login"),
 
     path('logout/',auth_views.LogoutView.as_view()
-    , name="logout")
+    , name="logout"),
 
 ]
